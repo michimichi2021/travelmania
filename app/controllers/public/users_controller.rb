1c5712d2ab3ef7b7b553a6742f0018efee964732
michimichi2021 <michikaho1009@gmail.com>
2021-10-19 17:43:15 +0000

    ActiveStorage #13

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,12 +1,20 @@
 class Public::UsersController < ApplicationController
 
 def show
+  @user = User.find(params[:id])
 end
 
 def edit
+  @user = User.find(params[:id])
 end
 
 def update
+  @user = User.find(params[:id])
+  if @user.update(user_params)
+    redirect_to user_path(@user)
+  else
+    render 'edit'
+  end
 end
 
 def favorites
@@ -26,5 +34,8 @@ end
 
 private
 
+def user_params
+  params.require(:user).permit(:name, :email, :is_deleted, :image, :introduction)
+end
 
 end
