f12795424fc76ac21be566c51ec514b058109df7
michimichi2021 <michikaho1009@gmail.com>
2021-10-20 10:06:11 +0000

    投稿、検索 #15 #16

--- Gemfile
+++ Gemfile
@@ -69,3 +69,7 @@ end
 gem 'devise'
 gem 'activestorage-validator'
 gem 'image_processing', '~> 1.2'
+gem 'font-awesome-sass', '~> 5.13'
+gem 'bootstrap', '~> 4.5'
+gem 'jquery-rails'
+gem 'ransack'
--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,23 +1,53 @@
 class Public::PostsController < ApplicationController
+before_action :search_product, only: [:search]
+  
   
 def new
+  @post = Post.new
 end
 
 def create
+  @post = current_user.posts.new(post_params)
+ 
+  if @post.save(post_params)
+   
+    redirect_to post_path(@post)
+  else
+    render 'new'
+  end
 end
 
 def show
+  @post = Post.find(params[:id])
 end
 
 def edit
+  @post = Post.find(params[:id])
 end
 
 def update
+  @post = Post.find(params[:id])
+  if @post.update
+    redirect_to post_path(@post)
+  else
+    render 'edit'
+  end
 end
 
 def search
+  @results = @p.result
+  @posts = Post.all
 end
 
 private
+
+def search_product
+  @p = Post.ransack(params[:q]) 
+  @results = @p.result
+end
+
+def post_params
+  params.require(:post).permit(:name, :introduction, :area_id, :genre_id, :scene_id, :image)
+end
 
 end
