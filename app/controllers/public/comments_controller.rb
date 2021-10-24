33d496bede394d750da96be6a8b46085ecc301d5
michimichi2021 <michikaho1009@gmail.com>
2021-10-24 16:32:34 +0000

    途中

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -2,20 +2,21 @@ class Public::CommentsController < ApplicationController
 
   def create
     @post = Post.find(params[:post_id])
-    comment = Comment.new(comment_params)
-    comment.post_id = @post.id
-    comment_post = comment.post
-    comment.id = comment.post.id
-    comment.save
-    comment_post.create_notification_comment!(current_user, comment.id)
+    @comment = Comment.new(comment_params)
+    @comment.user_id = current_user.id
+    @comment.post_id = @post.id
+    @comments = @post.comments
+    @comment_post = @comment.post
+    @comment.save
+    @comment_post.create_notification_comment!(current_user, @comment.id)
     redirect_to post_path(@post)
-  end
-  
+  end 
+
   def destroy
     Comment.find_by(id: params[:id]).destroy
     redirect_to post_path(params[:post_id])
   end
-  
+
   private
 
   def comment_params
