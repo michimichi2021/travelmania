771d63b57d44ddadb85e6df6f89553a7ec45d0c7
michimichi2021 <michikaho1009@gmail.com>
2021-10-26 14:57:08 +0000

    レイアウトトップ画面

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -2,7 +2,14 @@ class Public::HomesController < ApplicationController
 before_action :search_product, only: [:top]
 
 def top
- 
+  @posts = Post.order('id DESC').limit(5)
+  @post_ranking_tohokus = Post.post_favorite.where(area_id: 1).order_rank
+  @post_ranking_kantos = Post.post_favorite.where(area_id: 2).order_rank
+  @post_ranking_tyubus = Post.post_favorite.where(area_id: 3).order_rank
+  @post_ranking_kinkis = Post.post_favorite.where(area_id: 4).order_rank
+  @post_ranking_tyugokus = Post.post_favorite.where(area_id: 5).order_rank
+  @post_ranking_shikokus = Post.post_favorite.where(area_id: 6).order_rank
+  @post_ranking_kyusyus = Post.post_favorite.where(area_id: 9).order_rank
 end
 
 private
