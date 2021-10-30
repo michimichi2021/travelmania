86d800729db25dccb31abda55301491d9cb39674
michimichi2021 <michikaho1009@gmail.com>
2021-10-30 08:09:43 +0000

    お気に入り一覧 #61

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -26,6 +26,9 @@ def update
 end
 
 def favorites
+  @user = User.find(params[:id])
+  favorites = Favorite.where(user_id: @user.id).pluck(:post_id)
+  @favorite_posts = Post.find(favorites)
 end
 
 def follows
