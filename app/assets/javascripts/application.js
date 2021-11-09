af8e13ada928739f9fff2d37c51166ca170f1df6
michimichi2021 <michikaho1009@gmail.com>
2021-11-09 04:37:25 +0000

    トップページの画像

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,23 +18,3 @@
 //= require turbolinks
 //= require_tree .
 
-function slideSwitch() {
-   var $active = $('#slideshow img.active');
-
-   if ( $active.length == 0 ) $active = $('#slideshow img:last');
-
-   var $next =  $active.next().length ? $active.next()
-   : $('#slideshow img:first');
-
-   $active.addClass('last-active');
-
-   $next.css({opacity: 0.0})
-   .addClass('active')
-   .animate({opacity: 1.0}, 1000, function() {
-        $active.removeClass('active last-active');
-   });
-}
-
-$(function() {
-   setInterval( "slideSwitch()", 7000 );
-});
